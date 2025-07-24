e002f2f210a250ddc22e60dc366672bc5f1850b6
Andreas Roehler <andreas.roehler@online.de>
2025-07-24 21:30:20 +0200

    avsait-secrets-example.el
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

---
+++ avsait-secrets-example.el
@@ -0,0 +1,12 @@
+;;; avsait-secrets-example.el  -*- lexical-binding: t; -*-
+
+;;; Rename this file in avsait-secrets.el and edit the required token
+;;; according to your provider.
+
+(setq groq_pw "MY_groq_TOKEN")
+
+(setq deepSeek_pw "MY_deepSeek_TOKEN")
+
+(setq chatGPT_pw "MY_chatGPT_TOKEN")
+
+(provide 'avsait-secrets-example)
